b51cacd91fa7f6779b3fed04e05de8d19cdd21ec
Zachary Thompson <zachthompson406@gmail.com>
2021-05-17 20:18:13 -0700

    Change second_degree to main in r.sh

--- assign4/r.sh
+++ assign4/r.sh
@@ -9,7 +9,7 @@ rm *.o *.lis *.out
 echo "Assemble interview.asm."
 nasm -f elf64 -l interview.lis -o interview.o interview.asm
 
-echo "Compile second_degree.c using the gcc compiler."
+echo "Compile main.c using the gcc compiler."
 gcc -c -Wall -m64 -fno-pie -no-pie -o main.o main.c
 
 echo "Link the object files using the gcc linker."
